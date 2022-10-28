100bd0654ef8f6c3213f1a553b593ff58b71ab57
emreeken183425 <emreeken1834@gmail.com>
2022-10-28 23:59:33 +0300

    api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,77 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { useState } from "react";
+import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
 
 const Home = () => {
+  //inputun içindeki değer için state oluşturduk  searchText
+  const [searchText, setSearchText] = useState();
+  const [newData, setNewData] = useState();
+  const [readMore, setReadMore] = useState(false)
+  const newsDataFromApi = async () => {
+    const API_KEY = "f3bec0d572254c0c95fa46e72a065627";
+    let url = `https://newsapi.org/v2/everything?
+q=${searchText}&page=1&apiKey=${API_KEY}`;
 
+    try {
+      //   const response = await axios.get(url);
+      const { data } = await axios.get(url);
+      setNewData(data.articles);
+
+      console.log(newData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newsDataFromApi();
+    setSearchText("");
+  };
 
   return (
     <>
-    <div>
-        <form className="search  " /*</div>onSubmit={handleSubmit}*/ >
-        <input
-          type="search"
-          className="search-input  "
-          placeholder="Search news..."
-        //   onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="submit" className="btn btn-success  ">Search</button>
-      </form>      
-    </div>
-    {/* <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card> */}
-    <div className="card" >
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Açıklama</p>
-  </div>
-  <ul className="list-group list-group-flush">    
-    <li className="list-group-item">tarih</li>
-    <li className="list-group-item">kaynak </li>
-  </ul>
-  <div className="card-body">
-   
-  <button type="submit" className="btn btn-primary  ">Ayrıntılar</button>
-  </div>
-</div>
-    
-    </>
-  )
-}
+      <div>
+        <form className="search  " onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="search-input  "
+            placeholder="Search news..."
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button type="submit" className="btn btn-success  ">
+            Search
+          </button>
+        </form>
+      </div>
 
-export default Home
+      <div className=" cardContainer row  justify-content-evenly align-items-center">
+        {newData?.map((item, index) => {
+          return (
+            <div className="card col-sm-12 col-md-6 col-lg-4 p-3   " key={index}>
+              <img src={item.urlToImage}  className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{item.title} </h5>
+                <p className="card-text">{readMore ?  item.description : item.description.slice(0,100)+ "..."}
+                <button onClick={()=>setReadMore(!readMore) } >{readMore ? "show less" : "readmore" } </button>
+                </p>
+               
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{item.publishedAt.slice(0,10)} </li>
+                <li className="list-group-item">{item.source.name}  </li>
+              </ul>
+              <div className="card-body">
+                <button type="submit"   className="btn btn-primary  ">
+                  Ayrıntılar
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
